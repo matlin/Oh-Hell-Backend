c59092b06a7702a8ccddfbf56f7b4184c3a3555f
Matthew Linkous <matthew.linkous@gmail.com>
2017-04-09 20:33:53 -0400

    Added some user routes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
+});
+
+router.post('/register', (req, res, next) => {
+  //check req body for username, email, pw, etc and on success set cookie
+  res.cookie('user', 'value', {expire : new Date() + 9999}).send("Succesfully register");
+});
+
+router.post('/login', (req, res, next) => {
+  //check credentials then update cookie
+});
+
+app.get('/logout', (req,res, next) => {
+     clearCookie('cookie_name');
+     res.send('Succesfully logged out.');
 });
 
 module.exports = router;
