7202c7cc25c5b62eda0601a0a9413cb4b17a9081
Kevin Serrao <kserrao@middlebury.edu>
2017-04-11 13:43:59 -0400

    minor changes

--- routes/game.js
+++ routes/game.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var Game = require('../game/game.js')
+
+let activeGames = new Map();
+let idCounter = 0;
 
 router.get('/', function(req, res, next) {
   res.send('No game has been created.');
 });
 
 //change to post
-router.get('/create/', (req, res, next) {
+router.post('/create/', (req, res, next) {
+  let newGame = new Game();
+  activeGames[idCounter] = newGame;
+  idCounter++;
   res.send('Created new game');
 });
 
@@ -14,4 +21,14 @@ router.get('/:id'), (req, res, next) {
   //give game info for that player
 });
 
+router.put('/:id/bet', (req, res, next) {
+  req.playerID
+  req.amountBet
+  activeGames[id].bet(player, amount)
+
+});
+
+router.put('/:id/play', (req, res, next) {
+
+});
 module.exports = router;
