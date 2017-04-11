bb131398ca1560cd750a90964c5bd03032d858a0
Kevin Serrao <kserrao@middlebury.edu>
2017-04-11 14:04:59 -0400

    create game route written

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var Game = require('../game/game.js')
+let express = require('express');
+let router = express.Router();
+let Game = require('../game/game.js')
 
 let activeGames = new Map();
 let idCounter = 0;
@@ -10,18 +10,22 @@ router.get('/', function(req, res, next) {
 });
 
 //change to post
-router.post('/create/', (req, res, next) {
-  let newGame = new Game();
+router.post('/create/', (req, res, next) => {
+  let minPlayers = req.params.minPlayers;
+  let maxPlayers = req.params.maxPlayers;
+  let userID = req.cookies.userID;
+  let newGame = new Game(idCounter, minPlayers, maxPlayers)
+  newGame.addPlayer(userID);
   activeGames[idCounter] = newGame;
   idCounter++;
   res.send('Created new game');
 });
 
-router.get('/:id'), (req, res, next) {
+router.get('/:id'), (req, res, next) => {
   //give game info for that player
 });
 
-router.put('/:id/bet', (req, res, next) {
+router.put('/:id/bet', (req, res, next) => {
   req.playerID
   req.amountBet
   activeGames[id].bet(player, amount)
